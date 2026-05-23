2737e89957bf838ad93798d7f141e5f9ac9916d0
Keneth <akeneth87@.com>
2026-05-22 21:43:48 -0600

    Integrado menu principal antes del juego

--- Funciones/Principal.cpp
+++ Funciones/Principal.cpp
@@ -1,5 +1,6 @@
 #include "../Heders/Juego.h"
 #include "../Heders/Login.h"
+#include "../Heders/Menu.h"
 
 int main() {
     // Ventana solo para el login
@@ -11,6 +12,21 @@ int main() {
     ventana.close(); // Cierra la ventana del login
 
     if (!loginExitoso) return 0;
+
+    // MENU
+    sf::RenderWindow ventanaMenu(
+        sf::VideoMode(800, 600),
+        "Destructor Espacial - Menu"
+    );
+
+    Menu menu;
+
+    bool iniciarJuego = menu.ejecutar(ventanaMenu);
+
+    ventanaMenu.close();
+
+    if (!iniciarJuego)
+        return 0;
 
     // Juego crea su propia ventana internamente
     Juego juego;
